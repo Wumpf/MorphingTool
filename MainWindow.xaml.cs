66af8834adf3aed1122d881a5363f89ea3469607
Andreas Reich <r_andreas2@web.de>
2013-05-08 09:29:58 +0200

    added functional load image dialogs * minor ui improvement

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace MorphingTool
 {
@@ -22,6 +23,54 @@ namespace MorphingTool
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// eventhandler for clicking on the loadstartimage button
+        /// </summary>
+        private void OnClick_LoadStartImage(object sender, RoutedEventArgs e)
+        {
+            BitmapImage image = LoadImageFileDialog();
+            if (image != null)
+                StartImage.Source = image;
+        }
+
+        /// <summary>
+        /// eventhandler for clicking on the loadendimage button
+        /// </summary>
+        private void OnClick_LoadEndImage(object sender, RoutedEventArgs e)
+        {
+            BitmapImage image = LoadImageFileDialog();
+            if (image != null)
+                EndImage.Source = image;
+        }
+
+        /// <summary>
+        /// opens a file dialog to load a picture
+        /// </summary>
+        /// <returns>a new BitmapImage or null if nothing was loaded</returns>
+        private BitmapImage LoadImageFileDialog()
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "Select a picture";
+            openDialog.Filter = "All supported images|*.jpg;*.jpeg;*.png;*.gif;*.tiff;*.bmp|" +
+                                 "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                                 "Portable Network Graphic (*.png)|*.png|" +
+                                 "Graphics Interchange Format (*.gif)|*.gif|" +
+                                 "Bitmap (*.bmp)|*.bmp|" +
+                                 "Tagged Image File Format (*.tiff)|*.tiff";
+            if (openDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    return new BitmapImage(new Uri(openDialog.FileName));
+                }
+                catch(Exception exp)
+                {
+                    return null;
+                }
+            }
+            return null;
         }
     }
 }
